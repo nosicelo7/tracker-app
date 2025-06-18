8f05ceffcbaf3da61f877ac8ae5076da23a2382e
nosicelo <nosicelomangubane@gmail.com>
2025-06-18 10:13:08 +0200

    view screen kt file change

--- app/src/main/java/vcmsa/ci/trackerapp/ViewScreen.kt
+++ app/src/main/java/vcmsa/ci/trackerapp/ViewScreen.kt
@@ -11,29 +11,26 @@ import androidx.core.view.WindowInsetsCompat
 class ViewScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_view_screen)
+        //Get arrays from intent(passed from Main Screen)
+        val waterIntake = intent.getIntArrayExtra("waterIntake") ?: IntArray(7)
+        val exerciseInput = intent.getIntArrayExtra("exerciseInput") ?: IntArray(7)
+        val sleepMinutes = intent.getIntArrayExtra("sleepMinutes") ?: IntArray(7)
 
-    val correctData = intent.getIntExtra("correctData", 0)
-    val message = if (correctData > 0) {
-        "Congrats! You did well today"
+        val txtDetails = findViewById<TextView>(R.id.txtDetails)
+
+        val sb = StringBuilder()  //sb is a string builder
+        for (i in 0 until 7) {
+            sb.append("Day ${i + 1}: \n")
+            sb.append("Water Intake: ${waterIntake[i]} ml\n")
+            sb.append("Exercise: ${exerciseInput[i]} min\n")
+            sb.append(" Sleep - ${sleepMinutes[i]} minutes")
+        }
+        txtDetails.text = sb.toString()
+
+        val btnMainScreen = findViewById<Button>(R.id.btnMainScreen)
+        btnMainScreen.setOnClickListener {
+            finish()
+        }
     }
-    val txtWater = findViewById<TextView>(R.id.txtWater)
-    val txtExercise = findViewById<TextView>(R.id.txtExercise)
-    val txtSleep = findViewById<TextView>(R.id.txtSleep)
-
-    txtWater.text = ""
-    txtExercise.text = ""
-    txtSleep.text = ""
-    feedbackTextView = ""
-
-     btnView.setOnClickListener {
-         txtWater.text = "Total water intake for today: $correctData"
-         txtExercise.text = "Total exercise time for today: $correctData"
-         txtSleep.text = "Total sleep time for today: $correctData"
-     }
-     findViewById<Button>(R.id.btnMainScreen).setOnClickListener {
-         finish()
-     }
-  }
 }
